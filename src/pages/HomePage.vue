045c9705ae925a556c1d7066e48fcdab3b53bd07
Lane <lanericharddavis@gmail.com>
2021-04-24 13:11:05 -0600

    sat commit1

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-around">
     <div class="row">
-      <div class="col-4">
-        <h4> USER INFO </h4>
+      <div class="col-12 col-md-3 order-1 order-md-1">
+        <!-- <Account v-if="state.user.id === state.profile.creator.id" /> -->
+        <h4> USER INFO</h4>
         <button class="btn btn-primary">
           Edit Profile
         </button>
@@ -10,10 +11,10 @@
           Create Post
         </button>
       </div>
-      <div class="col-4">
+      <div class="col-11 col-md-6 order-3 order-md-2">
         <Posts v-for="post in state.posts" :key="post.id" :post="post" />
       </div>
-      <div class="col-4">
+      <div class="col-12 col-md-3 order-2 order-md-3">
         <Commercials v-for="commercial in state.commercials" :key="commercial.title" :commercial="commercial" />
       </div>
     </div>
@@ -25,6 +26,7 @@ import { reactive, computed, onMounted } from 'vue'
 import Notification from '../utils/Notification'
 import { postsService } from '../services/PostsService'
 import { commercialsService } from '../services/CommercialsService'
+// import { accountService } from '../services/AccountService'
 import { AppState } from '../AppState'
 
 export default {
@@ -33,11 +35,13 @@ export default {
     const state = reactive({
       posts: computed(() => AppState.posts),
       commercials: computed(() => AppState.commercials)
+      // account: computed(() => AppState.account)
     })
     onMounted(async() => {
       try {
         await postsService.getAll()
         await commercialsService.getAll()
+        // await accountService.getAccount()
       } catch (error) {
         Notification.toast('Error: ' + error, 'error')
       }
