a644974a95d2126dc0d657469d61e9cff4fb20f9
Lane <lanericharddavis@gmail.com>
2021-04-24 13:11:40 -0600

    commit?

---
+++ src/pages/ActiveProfilePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="profile-page">
+    <h1>Welcome to Profile Page!</h1>
+  </div>
+  <div class="card my-3 shadow">
+    <div class="row">
+      <!-- <Posts v-for="post in state.posts" :key="post.id ===" :post="post" /> -->
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ProfilePage',
+  props: {
+    activeProfile: {
+      type: Object,
+      required: true
+    }
+  },
+  setup() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
